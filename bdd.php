a0287611c3db6833905a28c22b574918e6727e95
morganetouati <morgane.touati@dartybox.com>
2017-05-02 15:21:04 +0200

    readme

--- bdd.php
+++ bdd.php
@@ -2,7 +2,7 @@
 try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=db_planning;charset=utf8', 'root', '');
-	// $bdd = new PDO('mysql:host=51.254.32.181;dbname=db_planning;charset=utf8', 'dbown_planning', 'F0r_Planning');
+	// $bdd = new PDO('mysql:host=vps346171.ovh.net:8443;dbname=db_planning;charset=utf8', 'dbown_planning', 'F0r_Planning');
 }
 catch(Exception $e)
 {
